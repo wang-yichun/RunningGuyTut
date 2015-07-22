0acab59e79fd24b511e5f52034574869935b4e7d
wang-yichun <yishionwang314@163.com>
2015-07-23 01:09:15 +0800

    WORKING WITH GENERATED CODE

--- Assets/RunningGuyGame/ViewModels/CharacterViewModel.cs
+++ Assets/RunningGuyGame/ViewModels/CharacterViewModel.cs
@@ -6,4 +6,18 @@ using UnityEngine;
 
 
 public partial class CharacterViewModel {
+    public override bool ComputeShouldMoveLeft()
+    {
+        return MovementIntention == MovementIntention.Left;
+    }
+
+    public override bool ComputeShouldMoveRight()
+    {
+        return MovementIntention == MovementIntention.Right;
+    }
+
+    public override bool ComputeShouldStop()
+    {
+        return MovementIntention == MovementIntention.Stop;
+    }
 }
--- Assets/RunningGuyGame/Views/CharacterAvatarView.cs
+++ Assets/RunningGuyGame/Views/CharacterAvatarView.cs
@@ -7,7 +7,18 @@ using UniRx;
 
 
 public partial class CharacterAvatarView {
-    
+
+    Vector3 speed = new Vector3(3, 0, 0);
+    protected Animator animator;
+    protected Transform art;
+
+    public override void Awake()
+    {
+        base.Awake();
+        art = transform.FindChild("CharacterArt");
+        animator = art.GetComponent<Animator>();
+    }
+
     /// Subscribes to the state machine property and executes a method for each state.
     public override void MovementStateChanged(Invert.StateMachine.State value) {
         base.MovementStateChanged(value);
@@ -15,13 +26,32 @@ public partial class CharacterAvatarView {
     
     public override void OnIdle() {
         base.OnIdle();
+
+        animator.SetInteger("State", 0);
     }
     
     public override void OnMoveLeft() {
-        base.OnMoveLeft();
+        Observable.EveryFixedUpdate()
+            .Subscribe(l => transform.Translate(-speed * Time.deltaTime))
+            .DisposeWhenChanged(Character.MovementStateProperty);
+
+        animator.SetInteger("State", 1);
+        art.localEulerAngles = new Vector3(0, 180, 0);
     }
     
     public override void OnMoveRight() {
-        base.OnMoveRight();
+        Observable.EveryFixedUpdate()
+            .Subscribe(l => transform.Translate(speed * Time.deltaTime))
+            .DisposeWhenChanged(Character.MovementStateProperty);
+
+        animator.SetInteger("State", 1);
+        art.localEulerAngles = new Vector3(0, 0, 0);
+    }
+
+    protected override MovementIntention CalculateMovementIntention()
+    {
+        if (Input.GetKey(KeyCode.A)) return MovementIntention.Left;
+        if (Input.GetKey(KeyCode.D)) return MovementIntention.Right;
+        return MovementIntention.Stop;
     }
 }
